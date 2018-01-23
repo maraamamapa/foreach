d423d1f2f721fd5bc63e0e8e11ea8d50706cc6a5
Ampa22 <ampa_margalef@hotmail.com>
2018-01-23 20:07:24 +0100

    100

--- multiplicar.php
+++ multiplicar.php
@@ -7,7 +7,7 @@ foreach ($tablas as $indice => $valor) {
 
 //Se recogen los elementos del array,se crea un bucle for para que saque los
 //modificacion1
-  for ($i=1; $i <=70 ; $i++) {
+  for ($i=1; $i <=100 ; $i++) {
       $operacion= $i * $valor;
       echo  "$valor x $i =". $operacion."<br>";
     }
